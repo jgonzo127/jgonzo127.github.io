c7a203180e4ff2eb5d42090b1b3884f242220340
Jordan Gonzales <jgonzo127@gmail.com>
2015-05-06 00:00:47 -0700

    removed unused files

--- javascripts/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(x) {
-	$(x).preventDefault();
-	  $(".options > p").on('click', function() {
-	    $(".text-container").find(this.class + "-content").show();
-	});
-  
-}) ;
